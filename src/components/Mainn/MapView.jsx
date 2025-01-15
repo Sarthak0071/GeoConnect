e733852a502efeda7319ddf230f39a53986e876d
Sarthak0071 <sarthak8618@gmail.com>
2025-01-15 12:19:06 +0545

    Tailwind css added

--- src/components/Mainn/MapView.jsx
+++ src/components/Mainn/MapView.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
--- src/components/Mainn/TouristPlacesList.jsx
+++ src/components/Mainn/TouristPlacesList.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 const TouristPlacesList = ({ touristPlaces, handleShowOnMap }) => {
