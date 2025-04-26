df6a52395b2dbffab88add3724e67ec760ce2751
Sarthak0071 <sarthak8618@gmail.com>
2025-04-26 10:26:29 +0545

    Refactor Popup component: remove unused CSS file, enhance UI with success icon, and update button text

--- src/components/Login/Popup.jsx
+++ src/components/Login/Popup.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import "./Popup.css";
+import React, { useEffect } from "react";
+import "./Login.css";
 
 const Popup = ({ message, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (onClose) onClose();
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
   return (
     <div className="popup">
       <div className="popup-inner">
+        <div className="popup-success">
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+          </svg>
+        </div>
         <p>{message}</p>
-        <button onClick={onClose}>Close</button>
+        {onClose && (
+          <button onClick={onClose}>Continue</button>
+        )}
       </div>
     </div>
   );
